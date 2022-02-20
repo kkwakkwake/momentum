db83e446e9f5e467a89d2ff130ca08a042fd875c
kkwakkwake <99951752+kkwakkwake@users.noreply.github.com>
2022-02-21 01:00:44 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,7 @@ const MY_API_KEY = '5249c8a8592e20dce5ab85aa6371ce8e';
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${MY_API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${MY_API_KEY}&units=metric`;
   fetch(url).then((response) => response.json())
     .then((data) => {
       const weather = document.querySelector('#weather span:first-child');
